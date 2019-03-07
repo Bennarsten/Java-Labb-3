ce82cb248bee60adbd4e31ab9a382997069b2265
rallys <fredrik.bryngelsson@gmail.com>
2019-03-07 16:07:23 +0100

    Ändrat i Entity0

--- src/Entity0.java
+++ src/Entity0.java
@@ -1,8 +1,16 @@
 public class Entity0 extends Entity
 {
+    private Packet pkt;
     // Perform any necessary initialization in the constructor
     public Entity0()
     {
+        int[] hej= new int[3];
+        hej[0] = 1;
+        hej[1] = 2;
+        pkt = new Packet(5,5,hej );
+        System.out.println("ETT PAKET: ");
+        System.out.println(pkt.getSource());
+
     }
 
     // Handle updates when a packet is received.  Students will need to call
